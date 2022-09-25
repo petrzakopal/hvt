832061892caad65207232ced830d9ca1a05d8fa5
petrzakopal <petr.zakopal@gmail.com>
2022-09-26 01:15:19 +0200

    🎆 Přidány basic loga.

--- elmobilita.cz/.next/static/PRAJWFqz2-CwYSqSdAcVO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/980-79a539a5daafdb64.js","static/chunks/pages/index-17cbaf67067b6df4.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ elmobilita.cz/.next/static/VK6gQ27eH5l401qFBEsKK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/980-79a539a5daafdb64.js","static/chunks/pages/index-2ce54ab116b8961d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
