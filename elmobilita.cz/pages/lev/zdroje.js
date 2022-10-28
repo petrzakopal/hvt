0416d35d38230bd3fbc409d59787ec7ff8545183
petrzakopal <petr.zakopal@gmail.com>
2022-10-29 01:59:41 +0200

    koloběžky

--- elmobilita.cz/pages/lev/zdroje.js
+++ elmobilita.cz/pages/lev/zdroje.js
@@ -29,7 +29,198 @@ export const metaData = {
 const schema = {};
 
 function Index() {
-	return <></>;
+	return (
+		<>
+			<SourceWrapper>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[1]</span>}
+					name={
+						"nařízení Evropského parlamentu a Rady (EU) č. 168/2013 ze dne 15. ledna 2013 o schvalování dvoukolových nebo tříkolových vozidel a čtyřkolek a dozoru nad trhem s těmito vozidly [online]. [vid. 2022-10-18]. Dostupné z: https://eur-lex.europa.eu/legal-content/CS/TXT/PDF/?uri=CELEX:32013R0168&from=SV"
+					}
+					id="narizeni-eu-168-2013"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[2]</span>}
+					name={
+						"Light electric vehicles (LEVs) | SWOV [online]. [vid. 2022-10-18]. Dostupné z: https://swov.nl/en/fact-sheet/light-electric-vehicles-levs"
+					}
+					id="lev-swov"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[3]</span>}
+					name={
+						"Battery University [online]. [vid. 2022-10-18]. Dostupné z: https://batteryuniversity.com/"
+					}
+					id="battery-university"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[4]</span>}
+					name={
+						"Electric motor - Wikipedia [online]. [vid. 2022-10-24]. Dostupné z: https://en.wikipedia.org/wiki/Electric_motor"
+					}
+					id="wikipedia-electric-motor"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[5]</span>}
+					name={
+						"Michael Faraday, Inventor of the Electric Motor [online]. [vid. 2022-10-24]. Dostupné z: https://www.thoughtco.com/michael-faraday-inventor-4059933"
+					}
+					id="michael-faraday"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[6]</span>}
+					name={
+						"EV Tech info Motors [online]. [vid. 2022-10-24]. Dostupné z: https://www.evworks.com.au/page/technical-information/ev-tech-info/motors/"
+					}
+					id="dc-animace"
+				/>
+
+				<SourceBlock
+					icon={<span className="text-primary-accent">[7]</span>}
+					name={
+						"THE HISTORY OF ELECTRIC MOTOR TECHNOLOGY: A JOURNEY THROUGH TIME | PCB Stator Technology [online]. [vid. 2022-10-24]. Dostupné z: https://pcbstator.com/news/history-electric-motor-technology-journey-through-time"
+					}
+					id="journey-through-time"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[8]</span>}
+					name={
+						"Faraday Motor – 1821 - MagLab [online]. [vid. 2022-10-24]. Dostupné z: https://nationalmaglab.org/education/magnet-academy/history-of-electricity-magnetism/museum/faraday-motor-1821"
+					}
+					id="maglab-faraday"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[9]</span>}
+					name={
+						"Nikola Tesla - Wikipedia [online]. [vid. 2022-10-24]. Dostupné z: https://en.wikipedia.org/wiki/Nikola_Tesla"
+					}
+					id="nikola-tesla"
+				/>
+
+				<SourceBlock
+					icon={<span className="text-primary-accent">[10]</span>}
+					name={
+						"Motor Control Permanent Magnet Sync Motor (PMSM) | Farnell [online]. [vid. 2022-10-24]. Dostupné z: https://cz.farnell.com/motor-control-permanent-magnet-sync-motor-pmsm-technology"
+					}
+					id="pmsm"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[11]</span>}
+					name={
+						"Overview: Synchronous Motors (PMSM & BLDC) [online]. [vid. 2022-10-24]. Dostupné z: https://www.linkedin.com/pulse/overview-synchronous-motors-pmsm-bldc-deependra-thakur"
+					}
+					id="pmsms-overview"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[12]</span>}
+					name={
+						"KRAJÁNEK VLADIMÍR. Návrh synchronního motoru s permanentními magnety pro pohon elektromobilu [online]. nedatováno [vid. 2022-10-24]. Dostupné z: https://otik.zcu.cz/bitstream/11025/22898/1/Diplomova%20prace_Vladimir%20Krajanek.pdf"
+					}
+					id="krajanek-synchronni-motor"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[13]</span>}
+					name={
+						"Who Invented the Scooter? [online]. [vid. 2022-10-24]. Dostupné z: https://unagiscooters.com/eu/news/2020/12/08/who-invented-the-scooter/"
+					}
+					id="who-invented-the-scooter"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[14]</span>}
+					name={
+						"The Autoped Was the World's First Scooter [online]. [vid. 2022-10-29]. Dostupné z: https://www.treehugger.com/autoped-was-worlds-first-scooter-4858489who-invented-the-scooter/"
+					}
+					id="lada-norman"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[15]</span>}
+					name={
+						"Wim Ouboter And His Micro Mobility Units Story - Electric Scooter shop [online]. [vid. 2022-10-24]. Dostupné z: https://electric-scooter.shop/wim-ouboter-and-his-micro-mobility-units-story/?fbclid=IwAR3ODZpq8oGLjh4B1qrYl4XAEE9SppCjpY-pqPFbSl5y4aIupI7033F6VLA"
+					}
+					id="wim-ouboter-electric-scooter"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[16]</span>}
+					name={
+						"Electric Scooter History - Invention, Evolution & Innovations– fluidfreeride.com [online]. [vid. 2022-10-24]. Dostupné z: https://fluidfreeride.com/blogs/news/electric-scooters-history?fbclid=IwAR0nnzWqZZvLI2IxYf_BOP3l1T_7PUkC7XYobHh4KLkukjo-GIVP-wTZHIk"
+					}
+					id="electric-scooter-history"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[17]</span>}
+					name={
+						"Peugeot to unveil its all new electric e-Vivacity scooter [online]. [vid. 2022-10-29]. Dostupné z: https://ecofriend.com/peugeot-to-unveil-its-all-new-electric-e-vivacity-scooter.html"
+					}
+					id="scoot-elec"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[18]</span>}
+					name={
+						"The History of Electric Scooters [online]. [vid. 2022-10-24]. Dostupné z: https://www.linkedin.com/pulse/history-electric-scooters-prashant-dedhia-negotiation-ninja-?fbclid=IwAR1J7hC5xbfY1YdIhHf7NhSVbQYtHVh3YisfgBQIdgUP3jTTy81N0sVqd8A"
+					}
+					id="history-electric-scooters"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[19]</span>}
+					name={
+						"Electric Scooter History—How It All Started | Apollo Scooters [online]. [vid. 2022-10-24]. Dostupné z: https://apolloscooters.co/blogs/news/electric-scooter-history-how-it-all-started?fbclid=IwAR1GFP1NWNHcXODORvDvLSlRnJzNDPEhdpJJC9Shhxfg6IcX_rEVUhivvYk"
+					}
+					id="apolloscooters-electric-scooter-history"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[20]</span>}
+					name={
+						"RAZOR SCOOTER timeline | Timetoast timelines [online]. [vid. 2022-10-24]. Dostupné z: https://www.timetoast.com/timelines/razor-scooter?fbclid=IwAR3GLlnFJ96jj6UpqMBNaKWiew8X7pr6KDPdNFZYDu363yVzHV7P_chKOk8"
+					}
+					id="razor-scooter-timeline"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[21]</span>}
+					name={
+						"MyWay - a cool ultraportable electric scooter [online]. [vid. 2022-10-24]. Dostupné z: https://www.ubergizmo.com/2011/09/myway/"
+					}
+					id="myway-scooter"
+				/>
+
+				<SourceBlock
+					icon={<span className="text-primary-accent">[22]</span>}
+					name={
+						"Lithium-ion Battery Costs and Market [online]. [vid. 2022-10-24]. Dostupné z: https://data.bloomberglp.com/bnef/sites/14/2017/07/BNEF-Lithium-ion-battery-costs-and-market.pdf"
+					}
+					id="bloomberglp-lithium-battery-costs"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[23]</span>}
+					name={
+						"What Were The First Electric Scooters? » Electric Scooter Guide [online]. [vid. 2022-10-24]. Dostupné z: https://electric-scooter.guide/blog/the-first-electric-scooters/"
+					}
+					id="electric-scooter-guide-what-were-first-electric-scooters"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[24]</span>}
+					name={
+						"V Praze začaly jezdit sdílené elektrické koloběžky Lime. Podívejte se, jak fungují - Lupa.cz [online]. [vid. 2022-10-24]. Dostupné z: https://www.lupa.cz/aktuality/v-praze-zacaly-jezdit-sdilene-elektricke-kolobezky-lime-podivejte-jak-funguji/"
+					}
+					id="lupa-v-praze-zacaly-jezdit-sdilene-elektricke-kolobezky-lime"
+				/>
+
+				<SourceBlock
+					icon={<span className="text-primary-accent">[25]</span>}
+					name={
+						"Elektrické koloběžky | Elektrokoloběžky | Alza.cz [online]. [vid. 2022-10-24]. Dostupné z: https://www.alza.cz/sport/elektricke-kolobezky/18859663.htm"
+					}
+					id="alza-elektricke-kolobezky"
+				/>
+				<SourceBlock
+					icon={<span className="text-primary-accent">[26]</span>}
+					name={
+						"Chopper X9 Road Approved Electric Scooter 45 km/h 3,000 Watt Scooter, 50 km range [online]. [vid. 2022-10-29]. Dostupné z: https://www.amazon.de/-/en/Chopper-X9-Approved-Electric-Scooter/dp/B08HNBM9KX"
+					}
+					id="amazon-chopper"
+				/>
+			</SourceWrapper>
+		</>
+	);
 }
 
 export default Index;
